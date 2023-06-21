eb732fc1d30268d91d767771e2bb1d1d7153bf12
Brandon Levinsky <blevinsky91@gmail.com>
2023-06-21 15:41:19 -0400

    Ex 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            var subject = Console.ReadLine();
+
+
+            switch (subject.ToLower())
+            {
+                case "Math":
+                    Console.WriteLine("Not quite");
+                    break;
+                case "History":
+                    Console.WriteLine("Not bad, but no");
+                    break;
+                case "English":
+                    Console.WriteLine("No chance!");
+                    break;
+                case "Spanish":
+                    Console.WriteLine("No bueno!");
+                    break;
+                case "Science":
+                    Console.WriteLine("You know it!");
+                    break;
+                default:
+                    Console.WriteLine("Keep guessing!");
+                    break;
+            }
         }
     }
 }
